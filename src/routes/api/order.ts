f51f379f233011d5e40c1ea7a641565ac05e554a
Jérémie Barrière <barriere.jeremie@gmail.com>
2022-09-22 22:09:44 +0200

    create commande !

--- src/routes/api/order.ts
+++ src/routes/api/order.ts
@@ -14,16 +14,61 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const express = require('express');
 import bodyParser from "body-parser";
 
-
 const api = Router();
+const endpointSecret = "whsec_XSb8D6vsHsC7VJ7NKBILxmc0eOWwVob1";
+
+const fulfillOrder = async (session,response) => {
+  const theSession = await stripe.events.retrieve(session.id)
+  console.log(theSession)
+  const command = await prisma.commande.create({
+    data: {
+      stripe_id: session.id,
+      number: "VBMX00000002",
+      userId: theSession.data.object.client_reference_id,
+      status: "ENCOURS",
+    }
+  })
+
+  const payload = JSON.parse(theSession.data.object.metadata.myBody2)
+  payload.forEach(async item => {
+    item.size === null && (item.size = "Unique")
+    await prisma.article.create({
+      data: {
+        size: item.size,
+        flocage: item.flocage,
+        quantity: item.quantity,
+        commandeId: command.id,
+        productId: item.id,
+      }
+    })
+    
+  })
+}
+
+
 
 api.post('/create-checkout-session',  async (req, res) => {
   let orderTable = [];
+  
   const myBody = JSON.parse(req.body)
-  const myBody2 = JSON.stringify(myBody)
   myBody.forEach(item => {
     orderTable.push({price: item.stripe, quantity: item.quantity})
   });
+
+  myBody.forEach(item => {
+    delete item.slug;
+    delete item.image;
+    delete item.description;
+    delete item.stripe;
+    delete item.name;
+    delete item.id;
+    console.log(item["price"])
+
+  })
+
+    console.log(myBody)
+    const myBody2 = JSON.stringify(myBody)
+    console.log(myBody2)
   const session = await stripe.checkout.sessions.create({
     line_items: orderTable,
     mode: 'payment',  
@@ -31,7 +76,7 @@ api.post('/create-checkout-session',  async (req, res) => {
     metadata: {
      myBody2
     },
-    client_reference_id: "da285cd7-4d6c-444d-b39e-c220c8ff4bda",
+    client_reference_id: "848f9b9a-90aa-4e29-bf2e-d9991e6f48a9",
 
     allow_promotion_codes: true,
     success_url: `http://localhost:3000/payement/success`,
@@ -42,27 +87,6 @@ api.post('/create-checkout-session',  async (req, res) => {
 });
 
 
-const endpointSecret = "whsec_XSb8D6vsHsC7VJ7NKBILxmc0eOWwVob1";
-
-const fulfillOrder = async (session,response) => {
-  // TODO: fill me in
-  const session4 = await stripe.events.retrieve(session.id)
-  console.log(session4)
-  const command = await prisma.commande.create({
-    data: {
-      stripe_id: session.id,
-      number: "VBMX00000001",
-      userId: session4.data.object.client_reference_id,
-      status: "ENCOURS",
-    }
-  })
-  console.log(command)
-
-
-
-
-
-}
 
 
   api.post('/webhook', bodyParser.raw({type: 'application/json'}), ({headers, body}, response) => {
